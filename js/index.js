ec2cef038da0aeee7df115841b25404378a5d8d1
Abisai <abisaiweb7@gmail.com>
2021-03-03 09:40:00 -0300

    Comentários de cada ponto do codigo index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 
+/*AQUI EU PEGO OS ARQUIVOS GEOJSON, E TRATO ELES.
+PRIMEIRO TODOS OS MUNICIPIOS E DEPOIS OS PINS PARA CADA MUNICIPIO*/
+
 function loadDadosMunicipios(){
 
     fetch("/js/geojson/area-municipios-ma-br.geojson")
@@ -7,11 +10,8 @@ function loadDadosMunicipios(){
     })
     .then(function(data) {
         L.geoJSON(data).addTo(map);
-        for(var mun of data.features) {
-            //console.log(mun.properties.name)
-             ;
-        }
-
+     
+    
     fetch("/js/geojson/pins-municipios-ma-br.geojson")
     .then(function(response) {
         return response.json();
@@ -19,7 +19,10 @@ function loadDadosMunicipios(){
     .then(function(pins) {
         L.geoJSON(pins).addTo(map);
         
-        
+        /*AQUI EU JOGO DENTRO DE GPSMARKER OS PINS
+        VIA ONEACHFEATURE PEGO OS DADOS DO GEOJSON E TRABALHO COM ELES
+        PARA O EFEITO DE OVER E EXIBIÇÃO DO POPUP*/
+
         gpsMarker = new L.geoJson(pins, {
         onEachFeature: function(Feature, layer) {
             
@@ -41,13 +44,14 @@ map.fitBounds(gpsMarker.getBounds(), {padding: [0, 0]});
         
     });
 
-//  add geojson  
+//ADICIONO O MEU GEOJSON DENTRO DE GEOLAYER E ADICIONO AO MAPA  
 geoLayer = L.geoJson(data).addTo(map),
 
-// color scale from http://colorbrewer2.org
+// AQUI DEFINO AS CORES DO MEU MAPA, POSSO DEFINIR UMA COR OU VARIAS
+//color scale from http://colorbrewer2.org
 colors = ['#db4a52'];
 
-// set base styles
+// AQUI DEFINO O ESTILO
 geoLayer.setStyle({
     width: 735,
     height: 860,
@@ -59,7 +63,9 @@ geoLayer.setStyle({
 
 updateColors();
 
-// sets color of each layer randomly
+// AQUI SETO A COR DE FORMA RANDOMICA, SE NA VARIAVEL COLORS TIVER APENAS UMA COR
+// O MAPA TERÁ APENAS ELA, MAS SE DEFINIR MAIS CORES, O MAPA TERA ESSAS CORES..
+// E IRÁ ALTERAR DE FORMA RANDOMICA AS CORES EM CADA MUNICIPIO DO MAPA.
 function updateColors() {
 
     geoLayer.eachLayer(function(layer) {
@@ -70,9 +76,6 @@ function updateColors() {
     setTimeout(updateColors, 1500);
 }
 
-/*Mouse */
-
-
 
 });
     
@@ -81,7 +84,7 @@ function updateColors() {
 loadDadosMunicipios()
 
 
-
+/*CHAMADA DO LEAFLET*/
 var map = new L.Map('map', {
     center: new L.LatLng(-5.00891349418116, -45.43778011911654),
     zoom: 6.5,
